b1509ee529320613f896d256d5de3faaccb290b5
hangy <hangy0313@gmail.com>
2014-07-19 00:24:23 +0800

    Initial Commit

---
+++ farm/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  farm
+//
+//  Created by hangy on 2014/7/19.
+//  Copyright (c) 2014年 hangy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
